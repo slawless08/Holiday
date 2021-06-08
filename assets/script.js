b438847080fbf39b0576a9aca7845f9696895469
slawless08 <samuel.l.lawless@gmail.com>
2021-06-07 21:29:40 -0400

    Created function to get location from users' IP address.

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+/*
+Grabs the country and country code from the user's IP address 
+
+Currently, countryCode does not get stored properly, need to figure out how to call the country code outside of the fetch
+*/
+$(document).ready(function () {
+
+function getLocation(){
+    fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=b16d49c2021b4770b4f70583a0ea01f0&fields=country,country_code')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(data){
+      console.log(data);
+      console.log(data.country_code);
+      var countryCode = data.country_code
+      console.log(countryCode); 
+      return countryCode;
+  });
+}
+getLocation();
+})
+
