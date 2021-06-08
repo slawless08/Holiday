26c57e5009ff955ec4b6da8598358de314443d3e
Bernadette McKnight <sissyhanks@yahoo.com>
2021-06-08 05:42:17 -0400

    date verification

--- assets/scriptcal.js
+++ assets/scriptcal.js
@@ -1,8 +1,31 @@
-var requestUrl = 'https://calendarific.com/api/v2/holidays?api_key=35103c5d8d12361a3b9ac8b97222182f16d7ea99&country=US&year=2019,national'
+var fun;
+
+var requestUrl = 'https://calendarific.com/api/v2/holidays?api_key=35103c5d8d12361a3b9ac8b97222182f16d7ea99&country=US&month=08&year=2021'
+
 
 fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)})
+    fun = (data.response.holidays);
+    console.log(fun);
+  }
+      );
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+// is entry a date or NaN
+var entry = "07/22/2021";
+
+var dateCheck = Date.parse(entry);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+function verDate(dateCheck) {
+  if (isNaN(dateCheck)) {
+    return 'try again';
+  }
+  return "happy Birthday";
+}
+
+console.log(verDate(dateCheck));
+
