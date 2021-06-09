75d4e292c672945f9d6011fedd2815d64573e0a3
slawless08 <samuel.l.lawless@gmail.com>
2021-06-09 18:50:39 -0400

    Will now store the country code into local storage

--- assets/script.js
+++ assets/script.js
@@ -5,19 +5,21 @@ Currently, countryCode does not get stored properly, need to figure out how to c
 */
 $(document).ready(function () {
 
-function getLocation(){
-    fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=b16d49c2021b4770b4f70583a0ea01f0&fields=country,country_code')
-  .then(function (response) {
-    return response.json();
+  function getLocation(){
+      fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=b16d49c2021b4770b4f70583a0ea01f0&fields=country,country_code')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        console.log(data.country_code);
+        var countryCode = data.country_code
+        console.log(countryCode); 
+
+        localStorage.setItem("Country-code", countryCode);
+        return countryCode;
+    });
+  }
+  getLocation();
   })
-  .then(function(data){
-      console.log(data);
-      console.log(data.country_code);
-      var countryCode = data.country_code
-      console.log(countryCode); 
-      return countryCode;
-  });
-}
-getLocation();
-})
 
